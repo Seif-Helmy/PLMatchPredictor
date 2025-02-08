0a92a007140f0720c1c9b8984812be33b9163d3e
Seif-Helmy <seifh2003@outlook.com>
2025-02-08 15:51:02 -0500

    ONly left to Comment Model and Actually make it good

--- cleaner.py
+++ cleaner.py
@@ -35,7 +35,6 @@ def clean_fixtures_table(csv_file):
 
     # The way the model is going to work is that we get the rolling averages of the past 10 games before each game
     # and use that to determine the output of the current game
-    predictors = ["Venue", "OppCode", "ClubCode"]
     predictors_to_be_rolled = ["GF", "GA", "Poss", "Sh", "SoT", "SoT%", "G/Sh", "G/SoT"]
     new_predictors_rolling = [f"{c}_Rolling" for c in predictors_to_be_rolled]
 
@@ -46,9 +45,10 @@ def clean_fixtures_table(csv_file):
     # Then compute ethe mean of certain column to get the tolling averages of the past 10 games
     # The Club Column gets removed, so we need to re add it and drop the level the group by columns
     past_fixtures_with_rolling = past_fixtures.groupby("Club", group_keys=True).apply(
-        lambda x: rolling_averages(x, predictors_to_be_rolled, new_predictors_rolling, 10), include_groups=False)
+        lambda x: rolling_averages(x, predictors_to_be_rolled, new_predictors_rolling, 6), include_groups=False)
     past_fixtures_with_rolling = past_fixtures_with_rolling.assign(Club=past_fixtures_with_rolling.index.get_level_values(0))
     past_fixtures_with_rolling = past_fixtures_with_rolling.droplevel("Club")
+
 
 
 
@@ -57,6 +57,17 @@ def clean_fixtures_table(csv_file):
     past_fixtures_with_rolling.index = range(past_fixtures_with_rolling.shape[0])
     past_fixtures_with_rolling = past_fixtures_with_rolling.drop(
         columns=['Comp', 'xG', 'xGA', 'Poss', 'Sh', 'SoT', 'SoT%', 'G/Sh', 'G/SoT'])
+
+
+
+    # Good test with the rolling average "game" parameter size for method 1
+    # the more you increase the number of games rolled, the more exponentially it removes rows
+    # print(past_fixtures.shape)
+    # print(past_fixtures["Club"].value_counts())
+    # print()
+    # print()
+    # print(past_fixtures_with_rolling.shape)
+    # print(past_fixtures_with_rolling["Club"].value_counts())
 
 
 
@@ -91,18 +102,20 @@ def standardize_opponent_names(df):
 
 
 
-
+# ROLLING AVERAGE FUNCTION HAS A LOT OF POWER OVER THE MODEL
+# THE DROP NA REMOVES ALOT OF MATCHES DEPENDING ON THE GAMES PARAMETER IN METHOD 1
+# METHOD 2 IS NOT ACCURATE BUT CLOSE ENOUGH. IT GIVES THE CLOSEST VALUE T THE GAME WHERE WE CAN'T COMPUTE
 # Calculates rolling averages of the statistics column into the rolling columns.
 # Puts data that is already grouped by club in chronological order to compute the averages of the past 10 games
 def rolling_averages(group, stats, rolling_cols, games=10):
     group = group.sort_values("Date")
-    rolling_stats = group[stats].rolling(games, closed="left").mean()
+    rolling_stats = group[stats].rolling(games, min_periods=1, closed="left").mean()
 
-    group[rolling_cols] = rolling_stats
-    return group.dropna(subset=rolling_cols)
+    # METHOD 1
+    # group[rolling_cols] = rolling_stats
+    # return group.dropna(subset=rolling_cols)
 
+    # METHOD 2
+    group[rolling_cols] = rolling_stats.bfill()
+    return group
 
-
-
-
-clean_fixtures_table("AllFixtures.csv")
--- model.py
+++ model.py
@@ -1,9 +1,10 @@
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score
 
 
 
-
+# TODO : Optimize and Improve Model
 def make_predictions(data, predictors):
 
     rf = RandomForestClassifier(n_estimators=100, random_state=42)
@@ -21,5 +22,10 @@ def make_predictions(data, predictors):
     print(precision)
     print(accuracy)
 
+
+
+make_predictions(pd.read_csv("CleanedAllFixtures.csv", index_col=0),
+                 ["Venue", "OppCode", "ClubCode", "GF_Rolling", "GA_Rolling", "Poss_Rolling", "Sh_Rolling",
+                  "SoT_Rolling", "SoT%_Rolling", "G/Sh_Rolling", "G/SoT_Rolling"])
 
 
