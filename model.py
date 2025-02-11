a6ac53b971e59fa11bb091a77fa5b96c0733343a
Seif-Helmy <seifh2003@outlook.com>
2025-02-11 10:56:12 -0500

    first draft of model

--- model.py
+++ model.py
@@ -4,29 +4,51 @@ from sklearn.metrics import accuracy_score, precision_score
 
 
 
-# TODO : Optimize and Improve Model
-# SCRAPER.PY AND CLEANER.PY ARE COMPLETE
+
+
 def make_predictions(data, predictors):
-
-    rf = RandomForestClassifier(n_estimators=100, random_state=42)
-
-    training = data[data["Date"] < "2024-07-30"]
-    test = data[data["Date"] > "2024-07-30"]
-
-    rf.fit(training[predictors], training["Result"])
-    prediction = rf.predict(test[predictors])
-
-    accuracy = accuracy_score(test["Result"], prediction)
-
-    precision = precision_score(test["Result"], prediction)
-
-    print(precision)
-    print(accuracy)
+    pl_data = pd.read_csv(data, index_col=0)
 
 
+    # Since the model takes data from the past to predict the future,
+    # we must divide the train/test split by chronological order
+    pl_data = pl_data.sort_values("Date")
 
-# make_predictions(pd.read_csv("CleanedAllFixtures.csv", index_col=0),
-#                  ["Venue", "OppCode", "ClubCode", "GF_Rolling", "GA_Rolling", "Poss_Rolling", "Sh_Rolling",
-#                   "SoT_Rolling", "SoT%_Rolling", "G/Sh_Rolling", "G/SoT_Rolling"])
+
+    # Here we use an 85%/15% train/test split
+    splitting_index = int(len(pl_data) * 0.85)
+    training_data = pl_data.iloc[:splitting_index]
+    test_data = pl_data[splitting_index:]
+
+
+    # Define the predictors X and the target variables Y
+    x_train, y_train = training_data[predictors], training_data["Result"]
+    x_test, y_test = test_data[predictors], test_data["Result"]
+
+
+    # Initialise the RandForrest model
+    rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
+    rf_model.fit(x_train, y_train)
+
+
+    # Test the model
+    predictions = rf_model.predict(x_test)
+
+
+    # Evaluate model
+    accuracy = accuracy_score(y_test, predictions)
+    precision = precision_score(y_test, predictions, average='binary')  # Adjust if multi-class
+
+
+    print(f"Accuracy: {accuracy:.4f}")
+    print(f"Precision: {precision:.4f}")
+
+
+
+
+
+make_predictions("CleanedAllFixtures.csv",
+                 ["Venue", "OppCode", "ClubCode", "GF_Rolling", "GA_Rolling", "Poss_Rolling", "Sh_Rolling",
+                  "SoT_Rolling", "SoT%_Rolling", "G/Sh_Rolling", "G/SoT_Rolling"])
 
 
